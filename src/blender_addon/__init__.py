e33a3ac3ca3560ee8da81e0919672086e8b175fa
Unknown <zocker1600@posteo.net>
2020-02-15 13:16:06 +0100

    new version 0.0.3

--- src/blender_addon/__init__.py
+++ src/blender_addon/__init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "author" : "zocker_160",
     "description" : "addon for importing and exporting Empire Earth CEM files",
     "blender" : (2, 81, 16),
-    "version" : (0, 0, 2),
+    "version" : (0, 0, 3),
     "location" : "File > Import",
     "warning" : "only import of CEM v2 files is supported (for now)",
     "category" : "Import-Export"
