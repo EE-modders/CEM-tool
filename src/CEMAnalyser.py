d15f841a97c9ee04c5e8a1f44765dde0c651aa2e
Unknown <zocker1600@posteo.net>
2020-02-25 17:38:20 +0100

    output additional info with CEMAnalyser

--- src/CEMAnalyser.py
+++ src/CEMAnalyser.py
@@ -17,23 +17,25 @@ result = list()
 def write_file(results: list()):
     header = [ 
 		"Model Name", 
-		"material name of last model", 
-		"number of CEM files", 
+		"number of CEM files",
+		"number child models",
 		"CEM version major", 
 		"CEM version minor",
+		"number tag points",
 		"number materials",
-		"number frames"
+		"number frames",
+		"number LOD levels"
 		]
     with open("!output.csv", "w") as csvfile:
         csvfile.write(",".join(header)+"\n")
 
         for res in results:
-            csvfile.write("%s,%s,%s,%s,%s,%s,%s\n" % (res[0], res[1], res[2], res[3], res[4], res[5], res[6]))
+            csvfile.write("%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7], res[8]))
 
 def get_num_CEM_parts(binary, start_pos=0, num_cem=1):
     cem_blob = BytesIO(binary)
     cem_blob.seek(start_pos)
-    cem_blob.read(8)
+    #cem_blob.read(8)
     name_length = int.from_bytes(cem_blob.read(4), byteorder='little', signed=False)
     material_name = cem_blob.read(name_length)[:-1]
     print(material_name)
@@ -45,6 +47,10 @@ def get_num_CEM_parts(binary, start_pos=0, num_cem=1):
     else:
         return get_num_CEM_parts(cem, start_pos=next_file+8, num_cem=num_cem+1)
 
+def get_num_CEM_parts2(binary):
+	CEM_parts = binary.split(cem_magic)[1:]
+	return len(CEM_parts)
+
 #os.chdir("models/")
 for f in os.listdir("."):
     if f.endswith(".cem"):
@@ -55,10 +61,14 @@ for f in os.listdir("."):
             else:
                 version_maj = int.from_bytes(cemfile.read(2), byteorder='little', signed=False)
                 version_min = int.from_bytes(cemfile.read(2), byteorder='little', signed=False)
-                cemfile.read(12)
+                cemfile.read(8)
+                num_tag_points = int.from_bytes(cemfile.read(4), byteorder='little', signed=False)                
                 num_materials = int.from_bytes(cemfile.read(4), byteorder='little', signed=False)
                 num_frames = int.from_bytes(cemfile.read(4), byteorder='little', signed=False)
-                num_cem, material_name = get_num_CEM_parts(cemfile.read(-1))
-                result.append( [ f, material_name, num_cem, version_maj, version_min, num_materials, num_frames ] )
+                num_child_models = int.from_bytes(cemfile.read(4), byteorder='little', signed=False)
+                num_lod_levels = int.from_bytes(cemfile.read(4), byteorder='little', signed=False)
+                cemfile.seek(0)
+                num_cem = get_num_CEM_parts2(cemfile.read(-1))
+                result.append( [ f, num_cem, num_child_models, version_maj, version_min, num_tag_points, num_materials, num_frames, num_lod_levels ] )
 
                 write_file(result)
