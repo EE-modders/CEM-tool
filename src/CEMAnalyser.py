e1b3473703f82abaf04eafb6ff939aed82fe36f0
zocker-160 <zocker1600@posteo.net>
2020-06-04 20:32:31 +0200

    show more info in CEManalyser

--- src/CEMAnalyser.py
+++ src/CEMAnalyser.py
@@ -21,6 +21,8 @@ def write_file(results: list()):
 		"number child models",
 		"CEM version major", 
 		"CEM version minor",
+        "number faces",
+        "number vertices",
 		"number tag points",
 		"number materials",
 		"number frames",
@@ -30,7 +32,7 @@ def write_file(results: list()):
         csvfile.write(",".join(header)+"\n")
 
         for res in results:
-            csvfile.write("%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7], res[8]))
+            csvfile.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7], res[8], res[9], res[10]))
 
 def get_num_CEM_parts(binary, start_pos=0, num_cem=1):
     cem_blob = BytesIO(binary)
@@ -61,7 +63,8 @@ for f in os.listdir("."):
             else:
                 version_maj = int.from_bytes(cemfile.read(2), byteorder='little', signed=False)
                 version_min = int.from_bytes(cemfile.read(2), byteorder='little', signed=False)
-                cemfile.read(8)
+                num_faces = int.from_bytes(cemfile.read(4), byteorder='little', signed=False)
+                num_vertices = int.from_bytes(cemfile.read(4), byteorder='little', signed=False)
                 num_tag_points = int.from_bytes(cemfile.read(4), byteorder='little', signed=False)                
                 num_materials = int.from_bytes(cemfile.read(4), byteorder='little', signed=False)
                 num_frames = int.from_bytes(cemfile.read(4), byteorder='little', signed=False)
@@ -69,6 +72,6 @@ for f in os.listdir("."):
                 num_lod_levels = int.from_bytes(cemfile.read(4), byteorder='little', signed=False)
                 cemfile.seek(0)
                 num_cem = get_num_CEM_parts2(cemfile.read(-1))
-                result.append( [ f, num_cem, num_child_models, version_maj, version_min, num_tag_points, num_materials, num_frames, num_lod_levels ] )
+                result.append( [ f, num_cem, num_child_models, version_maj, version_min, num_faces, num_vertices, num_tag_points, num_materials, num_frames, num_lod_levels ] )
 
                 write_file(result)
